6a5bb1f4d66ced5c4463a5021a29b7df88ef3a8f
Sunny Sun <newzealandsunny@gmail.com>
2021-06-25 08:06:14 +1200

    update Two way binding

--- section4_react-state/src/components/NewExpense/ExpenseForm.js
+++ section4_react-state/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //   const [enteredTitle, setEnteredTitle] = useState('');
   //   const [enteredAmount, setEnteredAmount] = useState('');
   //   const [enteredDate, setEnteredDate] = useState('');
@@ -49,10 +49,10 @@ const ExpenseForm = () => {
         amount: userInput.enteredAmount,
         date: new Date(userInput.enteredDate)
     }
+    props.onSaveExpenseData(expenseData);
     setUserInput((prevState) => {
         return {...prevState, enteredDate:"", enteredAmount:"", enteredTitle:""}
     });
-    console.log(expenseData);
   };
 
   return (
