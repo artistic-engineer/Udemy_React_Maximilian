b250626ae08c3fa8fb117b61afd2b81f5dc91ff1
Sunny Sun <newzealandsunny@gmail.com>
2021-07-03 13:42:06 +1200

    create new project for chapter 5

---
+++ section5_rendering_lists-conditional_content/src/components/Expenses/Expense.js
@@ -0,0 +1,44 @@
+import "./Expense.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import React, {useState} from "react";
+
+const Expense = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+    console.log(filteredYear);
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expense;
