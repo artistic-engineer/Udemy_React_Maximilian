fc4dd78e277cde765d9ad91df718a4e3c107cf1f
Sunny Sun <newzealandsunny@gmail.com>
2021-07-03 19:14:16 +1200

    finish the assignment 3

--- section5_rendering_lists-conditional_content/src/components/Expenses/Expense.js
+++ section5_rendering_lists-conditional_content/src/components/Expenses/Expense.js
@@ -2,43 +2,43 @@ import "./Expense.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Expense = (props) => {
-
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
     console.log(filteredYear);
-  }
+  };
+
+  //Add filtered Result.
+  const filteredExpress = props.items.filter(expenses => {
+    return expenses.date.getFullYear().toString() === filteredYear;
+  })
 
   return (
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
+      <ExpensesFilter
+        onChangeFilter={filterChangeHandler}
+        selected={filteredYear}
       />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      
+      {filteredExpress.map(expense => (
+        <ExpenseItem 
+         key={expense.id}
+         title={expense.title} 
+         amount={expense.amount} 
+         date={expense.date}
+         />
+      ))}
     </Card>
   );
-}
+};
 
 export default Expense;
+
+
+
+
--- section5_rendering_lists-conditional_content/src/components/Expenses/ExpenseItem.js
+++ section5_rendering_lists-conditional_content/src/components/Expenses/ExpenseItem.js
@@ -5,20 +5,20 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
   
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-  }
+  // const clickHandler = (preState) => {
+  //   setTitle('Updated!');
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
