a84927e085410d67174b0584e6d170e45dbb59b9
Sunny Sun <newzealandsunny@gmail.com>
2021-07-03 23:57:56 -0400

    Finish section 5

--- section5_rendering_lists-conditional_content/src/components/Chart/ChartBar.js
+++ section5_rendering_lists-conditional_content/src/components/Chart/ChartBar.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./CharBar.css";
+import "./ChartBar.css";
 
-const CharBar = (props) => {
+const ChartBar = (props) => {
 
   let barFillHeight = '0%';
-  if(props.max > 0) {
+  if(props.maxValue > 0) {
       barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%'; 
   }
   
@@ -17,3 +17,5 @@ const CharBar = (props) => {
     </div>
   );
 };
+
+export default ChartBar;
--- section5_rendering_lists-conditional_content/src/components/Expenses/Expense.js
+++ section5_rendering_lists-conditional_content/src/components/Expenses/Expense.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expense = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -26,6 +27,7 @@ const Expense = (props) => {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
+      <ExpensesChart expenses={filteredExpress} />
       <ExpensesList items={filteredExpress} />
     </Card>
   );
